e46a45ca8bccbf907e0a1ccc099f6cd7b4eda3f0
Gregory Singleton <yadig69design@gmail.com>
2020-08-18 04:37:25 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -3,5 +3,8 @@ def find_element_index(array, value_to_find)
   count = 0 
   while count < array.length do
     if array[count] == value_to_find
-      return
+      return count
+    end
+    
+  end
 end
